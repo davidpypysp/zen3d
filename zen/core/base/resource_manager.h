c8b774243256eef806544cf760da51e7ae85a842
dv <davidpy1992@gmail.com>
2023-08-27 22:25:03 -0700

    resource manager

--- zen/core/base/resource_manager.h
+++ zen/core/base/resource_manager.h
@@ -6,28 +6,32 @@
 
 namespace zen {
 
+typedef uint32_t ResourceHandle;
+
 template <typename T> class ResourceManager {
 public:
   ResourceManager() {}
 
-  template <typename... Args> uint32_t Create(Args... args) {
-    uint32_t handle = created_resource_count_++;
+  template <typename... Args> inline ResourceHandle Create(Args... args) {
+    ResourceHandle handle = GenerateHandle();
     resources_[handle] = std::make_unique<T>(args...);
     return handle;
   }
 
-  T *Get(const uint32_t handle) const {
+  inline T *Get(const ResourceHandle handle) const {
     if (resources_.find(handle) == resources_.end()) {
       return nullptr;
     }
     return resources_.at(handle).get();
   }
 
-  void Remove(const uint32_t handle) { resources_.erase(handle); }
+  inline void Remove(const ResourceHandle handle) { resources_.erase(handle); }
 
 protected:
+  inline ResourceHandle GenerateHandle() { return created_resource_count_++; }
+
   uint32_t created_resource_count_ = 0;
-  std::unordered_map<uint32_t, std::unique_ptr<T>> resources_;
+  std::unordered_map<ResourceHandle, std::unique_ptr<T>> resources_;
 };
 
 } // namespace zen
