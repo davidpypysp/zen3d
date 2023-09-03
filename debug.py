46a10d2a16b53faf28185af351ded44150fd8a35
dv <davidpy1992@gmail.com>
2023-09-03 15:11:29 -0700

    debug.py

---
+++ debug.py
@@ -0,0 +1,79 @@
+
+import subprocess
+import argparse
+import json as json
+import logging
+from typing import List
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("debug.py")
+
+def build_debug_target(target: str):
+    return subprocess.call(['bazel', '--output_base=bazel-debug', 'build', target, '--compilation_mode=dbg', '--verbose_failures'])
+
+def get_target_path(target: str):
+    return target.replace('//', '').replace(':', '/')
+
+def generate_debug_config(target: str):
+     return {
+        "name": "//zen/demo:gltf_model_loader_demo",
+        "type": "cppdbg",
+        "request": "launch",
+        "program": "${workspaceFolder}/bazel-debug/execroot/zen/bazel-out/k8-dbg/bin/" + get_target_path(target),
+        "args": [],
+        "stopAtEntry": True,
+        "cwd": "${workspaceFolder}",
+        "environment": [],
+        "externalConsole": False,
+        "MIMode": "gdb",
+        "setupCommands": [
+            {
+                "description": "Enable pretty-printing for gdb",
+                "text": "-enable-pretty-printing",
+                "ignoreFailures": True
+            }
+        ]
+     }
+
+def write_launch_json(config):
+    launch_json = {}
+    with open('.vscode/launch.json', 'r') as f:
+        launch_json = json.load(f)
+    with open('.vscode/launch.json', 'w') as f:
+        if 'configurations' not in launch_json:
+            launch_json['configurations'] = []
+
+        configs: List = launch_json['configurations']
+        for launch_config in configs:
+            if launch_config['name'] == config['name']:
+                print('overwriting existing config: ' + config['name'])
+                configs.remove(launch_config)
+        configs.append(config)
+        f.write(json.dumps(launch_json, indent=4))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                        prog='debug_bazel_target',
+                        description='generate bazel target debug config in .vscode/launch.json',
+                        epilog='Text at the bottom of help')
+    parser.add_argument('target')           # positional argument
+    args = parser.parse_args()
+
+    logger.info("building target: " + args.target)
+    signal = build_debug_target(args.target)
+    if signal != 0:
+        logger.error('build target failed: ' + args.target)
+        exit(1)
+
+    logger.info('build success')
+    config = generate_debug_config(args.target)
+    write_launch_json(config)
+    logger.info('debug target generated in .vscode/launch.json: ' + args.target)
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -15,6 +15,15 @@ npm install -g @bazel/buildifier
 * install gcc11 g++11
 * c++17
 
+### Debug target
+
+```
+# generate target vscode debug config in launch.json
+python3 debug.py //zen/demo:gltf_model_loader_demo
+```
+
 
 ## run wasm server with dir and port (requires python3.7)
+```
 python3 wasm_server.py ./bazel-bin/zen/demo/imgui_demo_wasm/ -p 8080
+```
