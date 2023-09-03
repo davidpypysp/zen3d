a9d163a5846c744bcbcf91e4bf541028dcf76e0b
dv <davidpy1992@gmail.com>
2023-09-02 22:01:24 -0700

    fix

--- zen/core/renderer/cube_geometry_test.cc
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "zen/core/renderer/geometry.h"
-
-TEST(CubeGeometryTest, BasicAssertions) { zen::CubeGeometry geometry; }
--- zen/core/renderer/mesh_flat_material_test.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "zen/core/renderer/mesh_flat_material.h"
-
-// Demonstrate some basic assertions.
-TEST(MeshFlatMaterialTest, BasicAssertions) {
-  zen::MeshFlatMaterial mesh_flat_material;
-}
